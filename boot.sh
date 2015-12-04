f9f4634315c4c8a642e5a624c2094c7bd6c31554
Mike Danese <mikedanese@google.com>
2015-12-03 18:37:59 -0800

    package build essentials and kernel modules in base image, and share a filesystem with host

--- boot.sh
+++ boot.sh
@@ -13,6 +13,8 @@ CMDLINE="root=/dev/sda console=ttyS0"
 DEBUG_OPTS="-S -gdb tcp::27467"
 MACADDR="52:54:00:$(dd if=/dev/urandom bs=512 count=1 2>/dev/null | md5sum | sed 's/^\(..\)\(..\)\(..\).*$/\1:\2:\3/')"
 
+SHARED_VOLUME="${HOME}"
+
 CPU_OPTS="-smp 2"
 MEMORY_OPTS="-m 2048"
 NET_OPTS="-netdev tap,id=net0 -device e1000,netdev=net0,mac=${MACADDR}"
@@ -34,5 +36,7 @@ kvm \
   -initrd "${INITRD}" \
   -hda    "${ROOTFS}" \
   -append "${CMDLINE}" \
+  -fsdev "local,security_model=passthrough,id=fsdev0,path=${SHARED_VOLUME}" \
+  -device "virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=hostshare" \
   ${NET_OPTS[@]} ${CPU_OPTS[@]} ${MEMORY_OPTS[@]} \
   -nographic ${EXTRA_OPTS[@]:-}
--- init.sh
+++ init.sh
@@ -12,10 +12,10 @@ rescue_shell() {
 mount -t proc none /proc
 mount -t sysfs none /sys
 mount -t devtmpfs none /dev
- 
+
 mount -o rw /dev/sda /mnt/root || rescure_shell
 
 umount /proc
 umount /sys
- 
+
 exec switch_root /mnt/root /sbin/init
