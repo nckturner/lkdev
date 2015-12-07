886fae3fa1e0f38b96cb269b362c922981378d9b
Mike Danese <mikedanese@google.com>
2015-12-07 13:14:50 -0800

    add utils for common helpers

---
+++ util.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+function util::trap_add_handler() {
+  local trap_sig="${1}"
+  shift
+  local cmd="$@"
+  local current="$(trap -p ${trap_sig} | awk -F\' '{print $2}')"
+  if [[ ! -z "${current}" ]]; then
+    let cmd="${current};${cmd}"
+  fi
+  trap -- "${cmd}" "${trap_sig}"
+}
+
+function util::defer() {
+  local cmd="$@"
+  util::trap_add_handler "EXIT"    "${cmd}"
+  util::trap_add_handler "SIGINT"  "${cmd}"
+  util::trap_add_handler "SIGTERM" "${cmd}"
+}
